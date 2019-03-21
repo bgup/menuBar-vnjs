5126caabde70065dc876ee5d4c86091cad1348bf
biron_up <bayronuruena@gmail.com>
2019-03-20 21:57:50 -0500

    Menu bar responsive final

---
+++ main.js
@@ -0,0 +1,20 @@
+const navbarToggler=document.querySelector(".navbar-toggler");
+const navbarMenu=document.querySelector(".navbar ul");
+const navbarLinks=document.querySelectorAll(".navbar a");
+alert("hello word;");
+
+navbarToggler.addEventListener("click", navbarTogglerClick);
+
+function navbarTogglerClick(){
+    navbarToggler.classList.toggle("open-navbar-toggler");
+    navbarMenu.classList.toggle("open");
+}
+
+
+navbarLinks.forEach(element=>element.addEventListener("click", navbarLinkClick));
+
+function navbarLinkClick(){
+    if(navbarMenu.classList.contains("open")){
+        navbarToggler.click();
+    }
+}
